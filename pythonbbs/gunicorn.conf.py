1c41b6219e354286ac18697e12bee342947307f8
operatorhs <18112488219@163.com>
2023-03-19 17:12:00 +0800

    a

--- pythonbbs/gunicorn.conf.py
+++ pythonbbs/gunicorn.conf.py
@@ -1,7 +1,11 @@
 
 import multiprocessing
 
-bind = '0.0.0.0:8000'
+# ./nginx -c /usr/local/nginx/conf/nginx.conf 启动nginx
+# ./nginx -s stop 停止
+# ./nginx -s reload
+
+bind = '127.0.0.1:8000'
 workers = multiprocessing.cpu_count() * 2 + 1
 accesslog = '/var/log/pythonbbs/access.log'
 daemon = True
